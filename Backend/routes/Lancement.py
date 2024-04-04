0e0a89e63e1c09bdaaaec364c628abd78a2b678c
Caroline Zheng <zgcaroline@gmail.com>
2024-04-04 12:05:58 +0200

    connexion inscription

--- Backend/routes/Lancement.py
+++ Backend/routes/Lancement.py
@@ -1,14 +1,10 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
 from personne_route import personne_bp
 from recette_route import recette_bp
-from flask import Flask
-#from flask_cors import CORS
 
-
-app= Flask(__name__)
-#cors = CORS(app)
-
-
-
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def hello_world():
@@ -16,21 +12,6 @@ def hello_world():
 
 app.register_blueprint(recette_bp, url_prefix='/recette')
 app.register_blueprint(personne_bp, url_prefix='/personne')
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
--- Backend/routes/personne_route.py
+++ Backend/routes/personne_route.py
@@ -1,13 +1,19 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint, jsonify, request
 from pymongo import MongoClient
-import json
+from flask_bcrypt import Bcrypt
 
-
-con= MongoClient("mongodb://localhost:27017")
-db=con.SaeDB
-
+con = MongoClient("mongodb://localhost:27017")
+db = con.SaeDB
+bcrypt = Bcrypt()
 
 personne_bp = Blueprint('personne', __name__)
+
+
+
+#test route
+@personne_bp.route('/h')
+def hello_world():
+	return 'Hefdslldo, f!!'
 
 
 @personne_bp.route('/<string:personne_nom>',methods=['GET'])
@@ -28,4 +34,42 @@ def ajouter_ingredient(personne_nom):
 def vider_liste_course(personne_nom):
 	db.personne.update_one({"nom":personne_nom},{"$set":{"liste_course":[]}})
 	return jsonify({'message': 'maj succès'})
+
+@personne_bp.route('/inscription', methods=['POST'])
+def register():
+    data = request.get_json()
+    username =data.get('username')
+    password = data.get('password')
+    nom = data.get('nom')
+    listeIngredients = []
+    listeCourse = []
+
+    if db.personne.find_one({'username': username}):
+        return jsonify({'message': 'usermanme pris'}), 400
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    personne_data = {
+        'username': username,
+        'password': hashed_password,
+        'nom': nom,
+        'listeIngredients': listeIngredients,
+        'listeCourse': listeCourse
+    }
+    db.personne.insert_one(personne_data)
+
+    return jsonify({'message': 'inscription reussie'}), 201
+
+@personne_bp.route('/connexion', methods=['POST'])
+def connexion():
+    data = request.get_json()
+    username =data.get('username')
+    password = data.get('password')
+
+    user = db.personne.find_one({'username': username})
+
+    if user and bcrypt.check_password_hash(user['password'], password):
+        return jsonify({'message': 'connecté'}), 200
+    else:
+        return jsonify({'message': 'incorrect'}), 401
 
